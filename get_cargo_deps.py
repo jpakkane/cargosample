3367191d79a3098820d92422af66a4bde37d42af
Jussi Pakkanen <jpakkane@gmail.com>
2018-03-04 14:33:23 +0200

    Can build the whole shebang.

--- get_cargo_deps.py
+++ get_cargo_deps.py
@@ -15,8 +15,57 @@
 # limitations under the License.
 
 import subprocess, sys, os
+import pathlib
+
+header_templ = '''project('%s', 'rust', version : '%s')
+
+'''
+
+target_templ = '''%s_lib = static_library('%s',
+  '%s')
+
+'''
+
+dep_templ = '''%s_dep = declare_dependency(link_with : %s_lib)\n'''
+
+def get_crate_repo_url(crate_name, crate_version):
+    # Crates.io does not seem to provide a query API
+    # and the web site is 100% javascript so web scraping
+    # is impossible. Thanks.
+    if crate_name != 'itoa':
+        sys.exit('This demo script only supports the itoa crate.')
+    if crate_version != '0.3.4':
+        sys.exit('This demo script only supports version 0.3.4.')
+    return 'https://github.com/dtolnay/itoa.git'
+
+def create_checkout(crate_name, crate_version):
+    spdir = pathlib.Path('subprojects')
+    outdir = spdir / ('%s-%s' % (crate_name, crate_version))
+    if outdir.exists():
+        print('Output path %s already exists, not doing a checkout.' % outdir)
+        return outdir
+    crate_url = get_crate_repo_url(crate_name, crate_version)
+    subprocess.check_call(['git', 'clone', crate_url, str(outdir)])
+    subprocess.check_call(['git', 'checkout', crate_version], cwd=outdir)
+    return outdir
+
+def create_mesonfiles(crate_name, crate_version, outdir):
+    mfile = outdir / 'meson.build'
+    with mfile.open('w') as ofile:
+        ofile.write(header_templ % (crate_name, crate_version))
+        ofile.write(target_templ % (crate_name,
+                                    crate_name,
+                                    'src/lib.rs'))
+        ofile.write(dep_templ % (crate_name, crate_name))
+
+def convert_single(crate_name, crate_version):
+    outdir = create_checkout(crate_name, crate_version)
+    create_mesonfiles(crate_name, crate_version, outdir)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         sys.exit(sys.argv[0] + ' <name of dependency> <version of dependency>')
     (name, version) = sys.argv[1:]
+    if not os.path.exists('subprojects'):
+        sys.exit('Subprojects directory does not exist.')
+    convert_single(name, version)
--- prog.rs
+++ prog.rs
@@ -1,3 +1,15 @@
+extern crate itoa;
+
 fn main() {
-    println!("Nothing intresting here yet.");
+    // write to a vector or other io::Write
+    let mut buf = Vec::new();
+    let result = itoa::write(&mut buf, 128u64);
+    match result {
+        Err(e) => {
+            println!("Could not convert number: {}", e);
+        }
+        Ok(_f) =>  {
+            println!("Converted number: {:?}", buf);
+        }
+    }
 }
